7068a045c1d6601717127f152708063df2a73de6
Ravindra <ravingalaxy@yahoo.com>
2021-05-12 12:18:48 +0530

    Add files via upload

---
+++ penguin classification/penguins-clf-model.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+penguins = pd.read_csv("penguins.csv")
+
+df = penguins.copy()
+target = 'species'
+encode = ['sex','island']
+
+for col in encode:
+    dummy =pd.get_dummies(df[col], prefix=col)
+    df = pd.concat([df,dummy], axis=1)
+    del df[col]
+
+target_mapper = {'Adelie':0, 'Chinstrap':1, 'Gentoo':2}
+def target_encode(val):
+    return target_mapper[val]
+
+df['species'] = df['species'].apply(target_encode)
+# Dividing X and Y
+X = df.drop('species', axis = 1)
+Y = df['species']
+
+# preparing random forest model
+clf = RandomForestClassifier()
+clf.fit(X, Y)
+
+# Saving model
+pickle.dump(clf, open('penguins_clf_model.pkl','wb'))
